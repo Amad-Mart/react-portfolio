85fe68a6ef92a6fc1b090152facd6c2f94835200
Deus <amadeusrm97@gmail.com>
2022-10-06 09:49:51 -0400

    added links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import Logos from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -15,13 +16,31 @@ const Sidebar = () => (
             <NavLink exacty='true' activeClassName='active' to='/'>
                 <FontAwesomeIcon icon={faHome} color='#4d4d4e' />
             </NavLink>
-            <NavLink exacty='true' activeClassName='active' className='about-link' to='/about'>
+            <NavLink 
+              exacty='true' 
+              activeClassName='active' 
+              className='about-link' 
+              to='/about'>
                 <FontAwesomeIcon icon={faUser} color='#4d4d4e' />
             </NavLink>
-            <NavLink exacty='true' activeClassName='active' className='contact-link' to='/contact'>
+            <NavLink 
+              exacty='true' 
+              activeClassName='active' 
+              className='contact-link' 
+              to='/contact'>
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
         </nav>
+        <ul>
+          <li>
+            <a 
+              target='_blank' 
+              rel='noreferrer' 
+              href='https://www.linkedin.com/in/amadeus-martinez-538a961a5/'>
+              <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'/>
+            </a>
+          </li>
+        </ul>
     </div>
 )
 
