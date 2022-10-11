41f39e7b9990dca91c676667a48f153dcaa7dcfb
Deus <amadeusrm97@gmail.com>
2022-10-11 12:36:33 -0400

    css

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () =>{
     return (
-        <>
-            <Sidebar/>
-        </>
+      <div className='App'>
+        <Sidebar/>
+        <div className='page'>
+            <span className='tage top-tags'>&lt;body&gt;</span>
+
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+              &lt;body&gt;
+              <br/>
+              <span className='bottom-tag-html'></span>
+            </span>
+        </div>
+      </div>
     )
     
 }
