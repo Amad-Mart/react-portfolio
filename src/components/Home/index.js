b2be45b62ec9c48f6f91bda4eff783b1720c3a72
Deus <amadeusrm97@gmail.com>
2022-10-18 12:10:22 -0400

    animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,23 @@
 import LogoTitle from '../../assets/images/logo-a.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['a','m','a','d','e','u','s']
+  const jobArray = ['f','u','l','l',' ', 's','t','a','c','k',' ','d','e','v','e','l','o','e','p','e','r','.']
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>Hi, <br/> I'm
         <img src={LogoTitle} alt='developer'  />
-        madeus
+        <AnimatedLetters letterClass={letterClass}
+          strArray = {nameArray}
+          idx={15}
+        />
         <br/>
         Full Stack Developer
         </h1>
